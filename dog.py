6601438940bffa8538bfc7360cb7531548fcf189
JohnSV18 <69915500+JohnSV18@users.noreply.github.com>
2020-11-23 09:05:32 -0500

    Added hero and ability pages

--- dog.py
+++ dog.py
@@ -1,5 +1,11 @@
 # dog.py
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, breed):
         self.name = name
+        self.breed = breed
         print("dog initialized!")
+
+    def bark(self):
+        print('woof!')
+
+
